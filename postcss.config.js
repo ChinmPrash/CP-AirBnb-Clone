529820605ddb004841fc8ae0b0e195a8c4de7a55
Chinmay Prashanth <chinmprash@icloud.com>
2021-08-05 09:06:33 +0000

    Day 1 - Heroku

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,11 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
+
 
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
 
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   },
 }
